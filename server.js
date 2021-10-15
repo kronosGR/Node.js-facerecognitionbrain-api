ea83e09ba2cdb9372e0d3fcc3630b2051fce91a7
Katsanakis Georgios <geo.elgeo@gmail.com>
2021-10-15 10:13:46 +0200

    Added temp signin register ends

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// important for the req.body
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'kronos',
+      email: 'kronos@example.com',
+      password: '123456',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get('/', (req, res) => {
-  res.send('this is working')
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email ===database.users[0].email &&
+      req.body.password === database.users[0].password) {
+        res.json('success');
+      } else{
+        res.status(400).json('error logging in');
+      }
+});
+
+app.post('/register', (req, res)=>{
+  const {email, name, password} = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  })
+  res.json(database.users)
 })
 
-app.listen(3000, ()=> {
-  console.log('listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
 
 /*
 /
